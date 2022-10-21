615114a80a6242c20d8327c83457f5bfc6dc12c4
Byron Kenoly Ochieng <byronochieng01@gmail.com>
2022-10-21 00:16:30 -0700

    corrected task1,2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,14 +12,14 @@ int largest_number(int a, int b, int c)
 {
 	if (a > b && a > c)
 	{
-		return a;
+		return (a);
 	}
 	else if (b > a && b > c)
 	{
-		return b;
+		return (b);
 	}
 	else
 	{
-		return c;
+		return (c);
 	}
 }
