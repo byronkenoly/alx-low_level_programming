4a6a745ffa2422a557cb01265e3ff6f6a9f013fb
Byron Kenoly Ochieng <byronochieng01@gmail.com>
2022-10-28 14:03:33 -0700

    corrected task1,7

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,6 +5,7 @@
 * @dest: an input string
 * @src: an input string
 * @n: an input integer
+*
 * Return: A pointer to the resulting string
 */
 char *_strncat(char *dest, char *src, int n)
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,8 +14,12 @@ char *leet(char *s)
      	while (s[i])
 	{
 		for (j = 0; j < 10; j++)
+		{
 			if (s[i] == str[j])
+			{
 				s[i] = subs[j];
+			}
+		}
 		
 		i++;
 	}
