2142443277172d31dbd0a2b0741dce5927c16f4a
Byron Kenoly Ochieng <byronochieng01@gmail.com>
2022-11-07 13:37:13 -0800

    task0,3 corrected

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,11 +16,12 @@ int main(int argc, char *argv[])
 	{
 		product = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", product);
-		return (0);
 	}
 	else
 	{
 		printf("Error\n");
 		return (1);
+		break;
 	}
+	return (0);
 }
