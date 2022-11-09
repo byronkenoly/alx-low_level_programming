019ec6810b92ed75bc2e835799685704e8901548
Byron Kenoly Ochieng <byronochieng01@gmail.com>
2022-11-09 07:00:51 -0800

    task0,1,2,3,4

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* create_array - creates an array of char
+* @size: size of the array
+* @c: char to initialize with
+*
+* Return: character
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	
