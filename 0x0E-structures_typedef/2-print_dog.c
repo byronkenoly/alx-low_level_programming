00c6d2875a901e5f60fd8475c88c5b9a6ccfeac1
Byron Kenoly Ochieng <byronochieng01@gmail.com>
2022-11-15 14:30:35 -0800

    task2,5 modified dog.h

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+* print_dog - prints a struct dog
+* @d: dog structure to be printed
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if ((*d).name == NULL)
+	{
+		(*d).name == "(nil)";
+	}
+	if ((*d).owner == NULL)
+	{
+		(*d).owner == "(nil)";
+	}
+
+	printf("Name: %s\nAge: %0.6f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+* free_dog - function that frees dogs
+* @d: dog to be freed
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	if ((*d).name != NULL)
+		free((*d).name);
+
+	if ((*d).owner != NULL)
+		free((*d).owner);
+
+	free(d);
+}
