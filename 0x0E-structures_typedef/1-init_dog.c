89893def2608290f452f7a70044aa836a8d78407
Byron Kenoly Ochieng <byronochieng01@gmail.com>
2022-11-15 13:51:52 -0800

    task0,1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "main.h"
+#include "dog.h"
+
+/**
+* init_dog - initializes a variable of type dog
+* @d: struct to initialize
+* @name: dog name
+* @age: dog age
+* @owner: dog owner
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+* struct dog - basic dog structure
+* @name: dog name
+* @age: dog age
+* @owner: dog owner
+*
+* Description: dog properties
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
