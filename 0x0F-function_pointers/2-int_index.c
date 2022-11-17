20882b275526bcb01a8a98a00025a6cf995aca73
Byron Kenoly Ochieng <byronochieng01@gmail.com>
2022-11-17 13:35:02 -0800

    more betty style

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,10 +18,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 
 	for (i = 0; i < size; i++)
-	{
 		if (cmp(array[i]))
 			return (i);
-	}
 	
 	if (i == size)
 		return (-1);
