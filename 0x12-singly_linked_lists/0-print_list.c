9c3c3150e3c7ae900100967d92b3e56b406250c8
Byron Kenoly Ochieng <byronochieng01@gmail.com>
2022-11-29 01:48:51 +0300

    modified task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,14 +14,11 @@ size_t print_list(const list_t *h)
 	for (; h != NULL; i++)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-		}
+
+		h = h->next;
 	}
 
 	return (i);
