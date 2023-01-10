4b6000945a4890aa4bb020a68f351cd89b17b43a
Byron Ochieng <byronochieng01@gmail.com>
2023-01-10 03:43:08 +0300

    output correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 	else if (x == 0)
 		printf("Last digit of %d is %d and is 0\n", n, x);
 	else
-		printf("Last digit of %d is %d and is less than 6 and not zero\n", n, x);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
 
 	return (0);
 }
