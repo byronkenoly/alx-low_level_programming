9da5a9a4187b90d370331e2363d51a9d73fedd01
Byron Ochieng <byronochieng01@gmail.com>
2023-01-21 20:40:17 +0300

    task9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+/**
+  * strcpy - copies string pointed to by src
+  * @dest: destination
+  * @src: source
+  *
+  * Return: pointer to dest
+  */
+
+char *_strcpy(char *dest, char *src)
+{
+	unsigned int i = 0;
+	int count = 0;
+
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	count = i + 1;
+
+	*(dest + count) = '\0';
+
+	return (dest);
+}
