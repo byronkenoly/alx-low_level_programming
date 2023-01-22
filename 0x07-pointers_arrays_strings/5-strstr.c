ebdd67f29e3a380cd32e669ecfc4f426e15901e0
Byron Ochieng <byronochieng01@gmail.com>
2023-01-22 15:07:42 +0300

    task5&6

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * _strstr - locates a substring
+  * @haystack: string
+  * @needle: substring
+  *
+  * Return: pointer to beginning of located substring
+  */
+
+char *_strstr(char *haystack, char *needle)
+{
+	
