2d5958fa917851a7113a5cda64a264994455f426
Byron Ochieng <byronochieng01@gmail.com>
2023-02-03 21:44:53 +0300

    task0 corrections

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,8 +11,9 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned long int i;
 
-	table = (hash_table_t *) malloc(sizeof(hash_table_t) * size);
+	table = (hash_table_t *) malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
 		return (NULL);
@@ -21,9 +22,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t *));
 
 	if (table->array == NULL)
+	{
+		free(table);
 		return (NULL);
+	}
 
-	free(table);
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
 
 	return (table);
 }
