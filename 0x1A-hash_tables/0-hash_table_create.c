e1e6d4198c1b3ee5e3fbc2bb1f132772b382dcf6
Byron Ochieng <byronochieng01@gmail.com>
2023-02-03 21:35:42 +0300

    returning NULL if anything went wrong, free used to de-allocate memory

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,8 +14,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table = (hash_table_t *) malloc(sizeof(hash_table_t) * size);
 
+	if (table == NULL)
+		return (NULL);
+
 	table->size = size;
 	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t *));
+
+	if (table->array == NULL)
+		return (NULL);
+
+	free(table);
 
 	return (table);
 }
